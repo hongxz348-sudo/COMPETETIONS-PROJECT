ae6a09bf7f3b5a76e4875ffebd40e70ab225908b
Spong <93772116+Spongbobe@users.noreply.github.com>
2025-08-21 11:49:44 +0700

    Phone Screen Version

---
+++ main.js
@@ -0,0 +1,8 @@
+const goTop_Btn = document.getElementById("Anchor-btn");
+window.addEventListener("scroll",()=>{
+    if (window.scrollY===0) {      //check scrolling on  the screen
+        goTop_Btn.style.display = "none";
+    } else {
+        goTop_Btn.style.display = "block";
+    }
+});
